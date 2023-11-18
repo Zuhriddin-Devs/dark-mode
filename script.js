ddcbd86c5dc5e096fe4c332b4ca37e8d3dc9647d
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-11-18 18:15:24 +0500

    Added function for space key

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const elBody = document.querySelector('body');
 elCheckBox.addEventListener('click', () => {
     elBody.classList.toggle('dark-mode');
 });
+
+elCheckBox.addEventListener("click", function (e) {
+    if (e.key === " ") {
+        elBody.classList.toggle('dark-mode');
+    };
+});
